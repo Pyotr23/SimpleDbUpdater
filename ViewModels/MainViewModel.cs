b7a211b211bd4644ed22a9c3e6d1f02cdc493166
Pyotr <2chilavert3@mail.ru>
2019-09-27 23:12:54 +0300

    Code behind пуст.

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,15 +1,137 @@
-﻿using SimpleDbUpdater.ViewModels;
+﻿using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using SimpleDbUpdater.Realizations;
+using SimpleDbUpdater.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
+using WinForms = System.Windows.Forms;
 
 namespace SimpleDbUpdater.ViewModels
 {
     class MainViewModel : ViewModelBase
     {
-        public ICommand SetScriptFolderPath { get; }
+        private string _scriptsFolderPath;
+        private string _connectionString;
+        public ICommand SetScriptsFolderPath { get; }
+        public ICommand ExecuteScripts { get; }
+        public string ScriptsFolderPath
+        {
+            get => _scriptsFolderPath;
+            set => SetProperty(ref _scriptsFolderPath, value); 
+        }
+        public string ConnectionString
+        {
+            get => _connectionString;
+            set => SetProperty(ref _connectionString, value);
+        }
+        
+        public MainViewModel()
+        {
+            ExecuteScripts = new RelayCommand(
+                o => 
+                {                    
+                    var sqlFiles = GetSqlFilePaths();
+                    try
+                    {
+                        ExecuteAndDeleteNonQueryScripts(sqlFiles);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }                    
+                }, 
+                x => !(string.IsNullOrEmpty(ScriptsFolderPath) || string.IsNullOrEmpty(ConnectionString)));
+
+            SetScriptsFolderPath = new RelayCommand(o => ScriptsFolderPath = GetScriptsFolderPath());
+        }
+
+        private string GetScriptsFolderPath()
+        {
+            using (var dialog = new WinForms.FolderBrowserDialog())
+            {
+                var result = dialog.ShowDialog();
+                if (result == WinForms.DialogResult.OK)
+                    return dialog.SelectedPath;
+                else
+                    return string.Empty;
+            }
+        }
+
+        private string[] GetSqlFilePaths()
+        {            
+            var fileFullPaths = Directory.GetFiles(ScriptsFolderPath);
+            var fileNames = fileFullPaths.Select(f => new FileInfo(f).Name).ToArray();
+            var sqlFileNames = fileNames.Where(s => new FileInfo(s).Extension == ".sql").ToArray();
+            var sortedSqlFilePathes = sqlFileNames.Select(n => Path.Combine(ScriptsFolderPath, n)).ToArray();
+            return sortedSqlFilePathes;
+        }
+
+        private void ExecuteAndDeleteNonQueryScripts(string[] scriptPaths)
+        {
+            if (scriptPaths.Length != 0)
+            {
+                var sqlConnection = new SqlConnection(ConnectionString);
+                var server = new Server(new ServerConnection(sqlConnection));
+                string dbName = GetDbNameFromConnectionString();
+                for (int i = 0; i < scriptPaths.Length; i++)
+                {
+                    var filePath = scriptPaths[i];
+                    string script;
+                    using (var streamReader = new StreamReader(filePath))
+                    {
+                        script = streamReader.ReadToEnd();
+                    }
+                    script = ModifyScript(script, i, dbName);
+                    try
+                    {
+                        server.ConnectionContext.ExecuteNonQuery(script);
+                        File.Delete(scriptPaths[i]);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception($"{ex.Message}\nОшибка при выполнении скрипта {Path.GetFileName(filePath)}.");
+                    }
+                }
+                MessageBox.Show("Обновление базы данных окончено.");
+            }
+            else
+                MessageBox.Show("Скрипты не найдены.");
+        }
+
+        // Парсер сработает, если перед названием БД нет пробела, сразу равно.
+        private string GetDbNameFromConnectionString()
+        {
+            string[] connectionSubstrings = ConnectionString.Split(new char[] { '=', ';', }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            int dbNameIndex = -1;
+            for (int i = 0; i < connectionSubstrings.Length; i++)
+            {
+                if (connectionSubstrings[i].Contains("Database"))
+                {
+                    dbNameIndex = ++i;
+                    break;
+                }
+            }
+            return connectionSubstrings[dbNameIndex];
+        }
+
+        private string ModifyScript(string script, int scriptIndex, string newDbName)
+        {
+            string modifiedScript;
+            if (scriptIndex == 0)
+                modifiedScript = script.Replace("DatabaseName", newDbName);
+            else
+            {
+                var neededChars = script.SkipWhile(c => c != '/').ToArray();
+                modifiedScript = new string(neededChars);
+            }
+            return modifiedScript;
+        }
     }
 }
--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.SqlServer.Management.Common;
 using Microsoft.SqlServer.Management.Smo;
+using SimpleDbUpdater.ViewModels;
 using System;
 using System.Data.SqlClient;
 using System.IO;
@@ -17,120 +18,7 @@ namespace SimpleDbUpdater.Views
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void BtnStart_Click(object sender, RoutedEventArgs e)
-        {
-            string connectionString = tbxConnectionString.Text;            
-            if (!string.IsNullOrEmpty(connectionString))
-            {
-                var sqlFiles = GetSqlFilePaths();
-                try
-                {
-                    ExecuteAndDeleteNonQueryScripts(sqlFiles, connectionString);                    
-                }
-                catch(Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }           
-            }
-            else
-                MessageBox.Show("Заполните все поля.");
-        }
-
-        private void ExecuteAndDeleteNonQueryScripts(string[] scriptPaths, string connectionString)
-        {
-            if (scriptPaths.Length != 0)
-            {
-                var sqlConnection = new SqlConnection(connectionString);
-                var server = new Server(new ServerConnection(sqlConnection));
-                string dbName = GetDbNameFromConnectionString(connectionString);                
-                for (int i = 0; i < scriptPaths.Length; i++)
-                {
-                    var filePath = scriptPaths[i];
-                    string script;
-                    using (var streamReader = new StreamReader(filePath))
-                    {
-                        script = streamReader.ReadToEnd();
-                    }                        
-                    script = ModifyScript(script, i, dbName);
-                    try
-                    {
-                        server.ConnectionContext.ExecuteNonQuery(script);
-                        File.Delete(scriptPaths[i]);
-                    }
-                    catch (Exception ex)
-                    {
-                        throw new Exception($"{ex.Message}\nОшибка при выполнении скрипта {Path.GetFileName(filePath)}.");
-                    }
-                }
-                MessageBox.Show("Обновление базы данных окончено.");
-            }
-            else
-                MessageBox.Show("Скрипты не найдены.");
-        }
-
-        private string ModifyScript(string script, int scriptIndex, string newDbName)
-        {
-            string modifiedScript;
-            if (scriptIndex == 0)
-                modifiedScript = script.Replace("DatabaseName", newDbName);
-            else
-            {
-                var neededChars = script.SkipWhile(c => c != '/').ToArray();
-                modifiedScript = new string(neededChars);
-            }
-            return modifiedScript;
-        }
-
-        private string[] GetSqlFilePaths()
-        {
-            var currentDirectory = tbxScriptsFolder.Text;
-            var fileFullPaths = Directory.GetFiles(currentDirectory);
-            var fileNames = fileFullPaths.Select(f => new FileInfo(f).Name).ToArray();
-            var sqlFileNames = fileNames.Where(s => new FileInfo(s).Extension == ".sql").ToArray();
-
-            // Если скрипты именуются числом с постоянным количеством символов (001, 010, 205), то их пути создаются этой строчкой.
-            var sortedSqlFilePathes = sqlFileNames.Select(n => Path.Combine(currentDirectory, n)).ToArray();
-
-            // Если файлы именуются числом с переменным количеством символом (1_, 12_, 104_), то нужно переопределять сортировку так.
-            //var sortedSqlFileNames = sqlFileNames.OrderBy(name => { return GetScriptNumber(name); }).ToArray();              
-            //var sortedSqlFilePathes = sortedSqlFileNames.Select(n => Path.Combine(currentDirectory, n)).ToArray();
-            return sortedSqlFilePathes;
-        }
-
-        // Парсер сработает, если перед названием БД нет пробела, сразу равно.
-        private string GetDbNameFromConnectionString(string connectionString)
-        {            
-            string[] connectionSubstrings = connectionString.Split(new char[] { '=', ';', }, StringSplitOptions.RemoveEmptyEntries).ToArray();
-            int dbNameIndex = -1;
-            for (int i = 0; i < connectionSubstrings.Length; i++)
-            {
-                if (connectionSubstrings[i].Contains("Database"))
-                {
-                    dbNameIndex = ++i;
-                    break;
-                }
-            }
-            return connectionSubstrings[dbNameIndex];
-        }
-
-        private int GetScriptNumber(string fileName)
-        {
-            var charsBeforeUnderscore = fileName.TakeWhile(c => c != '_').ToArray();
-            string stringBeforeUnderscore = new string(charsBeforeUnderscore);
-            int intBeforeUnderscore = int.Parse(stringBeforeUnderscore);
-            return intBeforeUnderscore;
-        }
-
-        private void BtnPath_Click(object sender, RoutedEventArgs e)
-        {
-            using (var dialog = new WinForms.FolderBrowserDialog())
-            {
-                var result = dialog.ShowDialog();
-                if (result == WinForms.DialogResult.OK)
-                    tbxScriptsFolder.Text = dialog.SelectedPath;
-            }
-        }
+            DataContext = new MainViewModel();
+        }                 
     }
 }
